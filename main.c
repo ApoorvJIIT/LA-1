9cfdf57930df08fe5b6926c55763d6a6f317dbe7
ApoorvJIIT <149455768+ApoorvJIIT@users.noreply.github.com>
2025-07-26 04:56:54 +0000

    q2

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    int n;
+    scanf("%d", &n);
+    int *arr = (int*) calloc(n, sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+        total += arr[i];
+    }
+    printf("Average: %f\n", (double)total/n);
+    
+}
